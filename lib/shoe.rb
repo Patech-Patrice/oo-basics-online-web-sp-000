0be974034fbdc73f804c75675921a9bc8b5598fd
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-26 23:18:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,20 +7,20 @@ class Shoe
     @brand
   end
  
-  def author=(author)
-    @author = author
+  def color=(color)
+    @color = color
   end
  
-  def author
-    @author
+  def color
+    @color
   end
  
-  def page_count=(num)
-    @page_count = num
+  def size=(size)
+    @size = size
   end
  
-  def page_count
-    @page_count
+  def size
+    @size
   end
  
   def genre=(genre)
